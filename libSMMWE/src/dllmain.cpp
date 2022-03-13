9cce0c96e03b9067d478e70af0cfdfcd1eea7979
JustinGdnz <justin.gdnz@gmail.com>
2022-03-13 17:14:55 -0600

    Test commit

--- libSMMWE/src/dllmain.cpp
+++ libSMMWE/src/dllmain.cpp
@@ -1,4 +1,5 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
+// test
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 #include <Windows.h>
 #include <iostream>
